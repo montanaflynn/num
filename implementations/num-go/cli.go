2a8c4a1109edb4f6c1857b4db34aff04e85283a3
Montana Flynn <montana@montanaflynn.me>
2015-11-21 23:44:37 -0800

    [wip] stdin and arg based data

--- implementations/num-go/cli.go
+++ implementations/num-go/cli.go
@@ -2,55 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/montanaflynn/stats"
+	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
+
+	"github.com/montanaflynn/stats"
 )
 
-func printResultOrError(result interface{}, err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(result)
-}
-
-func printAndExit(msg interface{}) {
-	fmt.Println(msg)
-	os.Exit(1)
-}
+// Num can take commands through cli and data
+// through both cli or piped in from stdin
+//
+// Here's the fucking logic:
+// 1) Check for commands
+// 2) Check for data through cli
+// 3) Check for data through stdin
+// 4) Calculate commands and display results
 
 func main() {
 
-	if len(os.Args) < 3 {
-		printAndExit("Missing function and data")
+	var data []float64
+	var sin = stdinData()
+	var arg = argData()
+
+	if len(os.Args) == 1 {
+		printAndExit("Missing commands, see --help")
 	}
+
+	if len(os.Args) == 2 && len(sin) == 0 {
+		printAndExit("Missing data, see --help")
+	}
+
+	data = append(data, sin...)
+	data = append(data, arg...)
 
 	var cmd = os.Args[1]
 
-	if len(os.Args) == 3 {
-		datum, err := strconv.ParseFloat(os.Args[2], 64)
-		if err != nil {
-			printAndExit(err)
-		}
+	if len(data) == 1 {
 		if cmd == "round" {
-			printResultOrError(stats.Round(datum, 0))
+			printResultOrError(stats.Round(data[0], 0))
 		}
 	}
 
-	if len(os.Args) > 3 {
-		var data []float64
-		for i := 0; i < len(os.Args[2:]); i++ {
-			datum, err := strconv.ParseFloat(os.Args[2:][i], 64)
-			if err == nil {
-				data = append(data, datum)
-			}
-		}
+	if len(data) > 1 {
 		if cmd == "sum" || cmd == "total" {
 			printResultOrError(stats.Sum(data))
 		}
 
-		if cmd == "mean" || cmd == "average" {
+		if cmd == "mean" || cmd == "average" || cmd == "avg" {
 			printResultOrError(stats.Mean(data))
 		}
 
@@ -62,4 +61,54 @@ func main() {
 			printResultOrError(stats.HarmonicMean(data))
 		}
 	}
+}
+
+func argData() (data []float64) {
+	for i := 0; i < len(os.Args[2:]); i++ {
+		datum, err := strconv.ParseFloat(os.Args[2:][i], 64)
+		if err == nil {
+			data = append(data, datum)
+		}
+	}
+	return
+}
+
+func stdinData() (data []float64) {
+
+	// let's just grab it all at once shall we
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return []float64{}
+	}
+
+	// Clean up any space and convert to a string
+	clean := strings.TrimSpace(string(stdin))
+
+	// break up the numbers into individual nums
+	pieces := strings.Split(clean, " ")
+
+	for i := 0; i < len(pieces); i++ {
+
+		// for each number we convert to float64
+		// ignoring any errors (non-data) we may find
+		datum, err := strconv.ParseFloat(pieces[i], 64)
+		if err == nil {
+			data = append(data, datum)
+		}
+	}
+
+	return
+}
+
+func printResultOrError(result interface{}, err error) {
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", result)
+}
+
+func printAndExit(msg interface{}) {
+	fmt.Printf("%+v\n", msg)
+	os.Exit(1)
 }
